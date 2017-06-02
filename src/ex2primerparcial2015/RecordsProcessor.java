4c19eae7d648ef64160aceb7f0732da33ddeb4f6
Marc Ribalta Gene <mariva1995@gmail.com>
2017-06-02 17:40:10 +0200

    little update

--- src/ex2primerparcial2015/RecordsProcessor.java
+++ src/ex2primerparcial2015/RecordsProcessor.java
@@ -7,4 +7,12 @@ public class RecordsProcessor {
     public RecordsProcessor(Coder c, Transform t){
 
     }
+
+    public String loadNext (){
+        return "";
+    }
+
+    public void save(String wannaSave){
+
+    }
 }
